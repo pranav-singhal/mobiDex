9c44d78b6a3ebe12abb4b0ce2729ca22c96585b9
Pranav Singhal <pranavsinghal96@gmail.com>
2019-02-03 14:48:36 +0530

    first commit

---
+++ src/components/ImportAccount.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+class ImportAccount extends React.Component {
+  render() {
+    return <Text>Hello import account!</Text>;
+  }
+}
+
+export default ImportAccount;
